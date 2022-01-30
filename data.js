e3938c37cd694d209ed58666ea3eec15f2756856
Ndeta Innocent <you@example.com>
2022-01-30 08:07:47 +0200

    Arrays

--- data.js
+++ data.js
@@ -28,6 +28,47 @@ strings.unshift('o') //O(n)
 // splice
 strings.splice(2,0, 'alien') //O(n)
 
+//BUILDING AN ARRAY
+class MyArray{
+    constructor(){
+        this.length=0 //initial length
+        this.data={}
+    }
+
+    get(index){
+        return this.data[index]
+    }
+    push(item){
+        this.data[this.length]=item
+        this.length++
+        return this.length
+    }
+    pop(){
+         const lastItem=this.data[this.length-1]
+         delete this.data[this.length-1]
+         this.length--
+         return lastItem
+    }
+    delete(index){
+      const item=this.data[index]
+      this.shiftItem(index)
+    }
+    shiftItem(index){
+      for(let i=index; i<this.length-1; i++){
+          this.data[index]=this.data[i+1]
+      }
+     delete this.data[this.length-1]
+     this.length--
+    }
+}
+
+const newArray=new MyArray()
+newArray.push('hi')
+newArray.push('you')
+newArray.push('0')
+newArray.pop()
+console.log(newArray)
+
 
 
 
