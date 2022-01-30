962a2ba76e9541e4b491e00ed6ff99ee044e3a57
Ndeta Innocent <you@example.com>
2022-01-30 07:40:51 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,39 @@
+// what is a data structure
+// IT IS A COLLECTIONS OF VALUES
+ 
+
+// INSERTION
+//DELETION
+//TRAVERSAL
+//SEARCHING
+//SORTING
+//MERGING
+//ACCESS
+
+// ARRAYS INTRODUCTION
+//arrays arranges items sequencially
+const strings=['a','b','c','d']
+strings[2]
+
+//push
+strings.push('e') // O(1)
+//pop
+strings.pop()
+strings.pop() //O(1)
+
+
+// unshift
+strings.unshift('o') //O(n)
+
+// splice
+strings.splice(2,0, 'alien') //O(n)
+
+
+
+
+
+
+
+
+
+console.log(strings)
