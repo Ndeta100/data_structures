96dad62663666f593420cac0f65ce372a9ae78fc
Ndeta Innocent <you@example.com>
2022-01-31 04:30:42 +0200

    google question

--- data.js
+++ data.js
@@ -189,4 +189,42 @@ myHashTable.set('grape', 10000)
 myHashTable.get('grapes')
 myHashTable.keys()
 
+// INTERVIEW QUESTION
 
+// google question
+//given an array=[2,51,2,3,5,1,2,4]
+//It should return 2
+
+//Given an array =[2,1,1,2,3,5,1,2,4]
+//it should return 1
+
+// Given an array=[2,3,4,5]
+// it should return undefined
+
+
+function firstRecurringCharacter(input){
+  for (let i=0; i<input.length; i++){
+    for (let j=i+1; j<input.length; j++){
+       if(input[i]===input[j]){
+           return input[i]
+       }
+    }
+  }
+  return undefined
+}
+
+// using a hash table
+function firstRecurringCharacter2(input){
+   let map={}
+   for(let i=0; i<input.length; i++){
+       if(map[input[i]]===undefined){
+           return input[i]
+       }else{
+        map[input[i]]=i
+    }
+    console.log(map)
+   }
+
+   return undefined
+}
+firstRecurringCharacter([2,51,2,3,5,1,2,4])
